5d3d3537c65308a47634e9dcd1ccd17b863e14d5
galabinangelann-ship-it <galabin.angelann@gmail.com>
2026-05-01 22:23:29 +0800

    append option

--- act.py
+++ act.py
@@ -1,9 +1,14 @@
 filename = "shop.txt"
 try:
     with open(filename,"x") as file:
-        print("File Created Successfully")
-        file.write("Welcom to your inventory ")
+            print("File Created Successfully")
 
 except:
     print("File Already exist")
 
+def add():
+        with open(filename,"a") as file:
+            additem = input("Enter item to add: ") 
+            file.write(additem + "\n")
+            
+    
